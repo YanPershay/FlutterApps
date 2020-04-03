77c5b90afe30ded403f9b2e3b2c40f611afe371a
YanPershay <yanik22827@gmail.com>
2020-04-04 01:30:13 +0300

    Format code

--- flutter_ui/lib/destination_screen.dart
+++ flutter_ui/lib/destination_screen.dart
@@ -25,125 +25,152 @@ class _DestinationScreenState extends State<DestinationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: <Widget>[
-          CustomScrollView(
-            slivers: <Widget>[
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: <Widget>[
+            CustomScrollView(slivers: <Widget>[
               SliverAppBar(
                 expandedHeight: 400,
                 backgroundColor: Colors.black,
                 flexibleSpace: FlexibleSpaceBar(
                   collapseMode: CollapseMode.pin,
                   background: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
                         image: AssetImage(widget.destination.imageUrl),
                         fit: BoxFit.cover,
-                      )
-                    ),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomRight,
-                          colors: [
-                            Colors.white,
-                            Colors.black.withOpacity(.3)
-                          ]
-                        )
-                      ),
-                      child:Padding(
-                        padding: EdgeInsets.all(20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            FadeAnimation(1, Text(widget.destination.city, style: 
-                            TextStyle(color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 40),
-                            )),
-                            SizedBox(height: 10,),
-                            Row(
-                              children: <Widget>[
-                                FadeAnimation(1.2,
-                                Text(widget.destination.country, style:
-                                TextStyle(color: Colors.black, fontSize: 18),)
-                                ),
-                              ],
-                            )
-                        ],)
-                      )
-                    )
-                  ),
+                      )),
+                      child: Container(
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  begin: Alignment.bottomRight,
+                                  colors: [
+                                Colors.white,
+                                Colors.black.withOpacity(.3)
+                              ])),
+                          child: Padding(
+                              padding: EdgeInsets.all(20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  FadeAnimation(
+                                      1,
+                                      Text(
+                                        widget.destination.city,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 40),
+                                      )),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    children: <Widget>[
+                                      FadeAnimation(
+                                          1.2,
+                                          Text(
+                                            widget.destination.country,
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 18),
+                                          )),
+                                    ],
+                                  )
+                                ],
+                              )))),
                 ),
               ),
               SliverList(
                 delegate: SliverChildListDelegate([
-                Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    FadeAnimation(1.2, Text(widget.destination.description,
-                    style: TextStyle(color: Colors.black, height:1.4),)),
-                    SizedBox(height: 40,),
-                    FadeAnimation(1.2, 
-                      Text("Photos", style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30),),
-                      ),
-                    SizedBox(height: 20,),
-                    FadeAnimation(1.2, 
-                    Container(
-                      height: 300,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: <Widget>[
-                          showPhoto(image: 'assets/images/hotel1.jpg'),
-                          showPhoto(image: 'assets/images/hotel2.jpg'),
-                          showPhoto(image: 'assets/images/murano.jpg')
-                        ],
-                      )
-                    )),
-                    SizedBox(height: 120,)
-                  ],
-                  ),
-                )
-              ]),
-              )
-            ]
-          ),
-          Positioned.fill(
-            bottom: 50,
-            child: Container(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: FadeAnimation(2,
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 30),
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.orange
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        FadeAnimation(
+                            1.2,
+                            Text(
+                              widget.destination.description,
+                              style:
+                                  TextStyle(color: Colors.black, height: 1.4),
+                            )),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        FadeAnimation(
+                          1.2,
+                          Text(
+                            "Photos",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        FadeAnimation(
+                            1.2,
+                            Container(
+                                height: 300,
+                                child: ListView(
+                                  scrollDirection: Axis.horizontal,
+                                  children: <Widget>[
+                                    showPhoto(
+                                        image: 'assets/images/hotel1.jpg'),
+                                    showPhoto(
+                                        image: 'assets/images/hotel2.jpg'),
+                                    showPhoto(image: 'assets/images/murano.jpg')
+                                  ],
+                                ))),
+                        SizedBox(
+                          height: 120,
+                        )
+                      ],
                     ),
-                    child: Align(child: Text("Follow", style: TextStyle(color: Colors.white),)),
-                  ),),),),),],
-      )
-    );
+                  )
+                ]),
+              )
+            ]),
+            Positioned.fill(
+              bottom: 50,
+              child: Container(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: FadeAnimation(
+                    2,
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 30),
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.orange),
+                      child: Align(
+                          child: Text(
+                        "Follow",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 
-  Widget showPhoto({image}){
+  Widget showPhoto({image}) {
     return AspectRatio(
-      aspectRatio: 1/ 1.3,
+      aspectRatio: 1 / 1.3,
       child: Container(
         margin: EdgeInsets.only(right: 20),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image:DecorationImage(
-            image: AssetImage(image),
-            fit: BoxFit.cover
-          )
-        ),
+            borderRadius: BorderRadius.circular(20),
+            image:
+                DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
       ),
     );
   }
